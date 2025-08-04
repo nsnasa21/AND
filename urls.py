9dff723274821d726c2ae7aa7a8735a8e0dc50eb
nsnasa21 <nsnasa21@gmail.com>
2025-08-04 18:16:19 +0600

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,8 @@
+# urls.py (project level)
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('news_aggregator.urls')),
+]
