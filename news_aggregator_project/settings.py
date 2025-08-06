86d36c2381c82e03d44bf5d79cebf80692ca4815
nsnasa21 <nsnasa21@gmail.com>
2025-08-06 15:58:44 +0600

    Update settings.py

--- news_aggregator_project/settings.py
+++ news_aggregator_project/settings.py
@@ -79,12 +79,13 @@ if os.environ.get('RENDER', False):
         )
     }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
